882dbdb881784772fe7dc093c326d0b1ffc04eed
Andrew Boktor <aboktor@microsoft.com>
2024-03-01 11:32:57 -0800

    Microbenchmarks that we modified while on the call

--- src/SimpleReactComponent.tsx
+++ src/SimpleReactComponent.tsx
@@ -4,16 +4,19 @@ import { waste } from "./helpers";
 
 export function renderWithRaf(div: HTMLDivElement) {
   ReactDom.render(<Component text="Hello World" />, div);
-  waste(1, 'raf requested');
-  requestAnimationFrame(() => requestAnimationFrame(() => waste(1, 'raf execution')));
+  
+  // requestAnimationFrame(() => waste(1, 'raf execution'));
+  setTimeout(() => {
+    waste(0, "raf requested");
+    requestAnimationFrame(() => waste(0, "raf execution"));
+  }, 10);
 }
 
 export function renderWithoutRaf(div: HTMLDivElement) {
   ReactDom.render(<Component text="Hello World" />, div);
-  waste(1, 'no raf requested');
+  waste(0, "no raf requested");
 }
 
 function Component(props: { text: string }) {
   return <div>{props.text}</div>;
 }
-
--- src/runBenchmarks.ts
+++ src/runBenchmarks.ts
@@ -1,52 +1,67 @@
 import { renderWithRaf, renderWithoutRaf } from "./SimpleReactComponent";
 
 export default async function runBenchmarks(numRuns: number) {
-    const benchmarks = [
-        { name: 'renderWithoutRaf', fn: renderWithoutRaf },
-        { name: 'renderWithRaf', fn: renderWithRaf },
-    ];
+  const benchmarks = [
+    { name: "renderWithoutRaf", fn: renderWithoutRaf },
+    { name: "renderWithRaf", fn: renderWithRaf },
+  ];
 
-    const benchmarkDurationTotals = new Map([
-        ['renderWithoutRaf', 0],
-        ['renderWithRaf', 0]
-    ]);
+  const benchmarkDurationTotals = new Map([
+    ["renderWithoutRaf", 0],
+    ["renderWithRaf", 0],
+  ]);
 
-    const runAll = (name: string) => {
-        const parentDiv = document.createElement("div");
-        parentDiv.setAttribute("id", "parentDiv");
-        document.body.appendChild(parentDiv);
-        benchmarks.forEach((benchmark) => {
-          const start = performance.now();
-          performance.mark(`${benchmark.name}start`);
-          for (let i = 0; i < 10; i += 1) {
-            const div = document.createElement("div");
-            parentDiv.appendChild(div);
-            benchmark.fn(div);
-          }
-          const end = performance.now();
-          performance.measure(benchmark.name, `${benchmark.name}start`);
-          const div = document.createElement("div");
-          div.setAttribute("automation", "true");
-          const duration = end - start;
-          div.innerText = `${name} ${benchmark.name} ${duration}`;
-          document.body.appendChild(div);
-          console.log(benchmark.name, duration);
-          // Exclude the first run from the average
-          if (!name.includes('1')) {
-            benchmarkDurationTotals.set(benchmark.name, benchmarkDurationTotals.get(benchmark.name) + duration);
-          }
-        });
-    };
-    
-    for (let i = 1; i <= numRuns; i++) {
-        runAll(`Run ${i}`);
-    }
+  const yieldToBrowser = () =>
+    new Promise((resolve) => {
+      setTimeout(resolve, 50);
+    });
 
-    benchmarks.forEach((benchmark) => {
+  const runAll = async (name: string) => {
+    const parentDiv = document.createElement("div");
+    parentDiv.setAttribute("id", "parentDiv");
+    document.body.appendChild(parentDiv);
+    for (const benchmark of benchmarks) {
+      const start = performance.now();
+      performance.mark(`${benchmark.name}start`);
+      for (let i = 0; i < 100; i += 1) {
         const div = document.createElement("div");
-        div.setAttribute("automation", "true");
-        div.innerText = `${benchmark.name} average duration: ${benchmarkDurationTotals.get(benchmark.name)/(numRuns-1)}`;
-        console.log(`${benchmark.name} average duration: ${benchmarkDurationTotals.get(benchmark.name)/(numRuns-1)}`);
-        document.body.appendChild(div);
-    })
+        parentDiv.appendChild(div);
+        benchmark.fn(div);
+      }
+      const end = performance.now();
+      performance.measure(benchmark.name, `${benchmark.name}start`);
+      const div = document.createElement("div");
+      div.setAttribute("automation", "true");
+      const duration = end - start;
+      div.innerText = `${name} ${benchmark.name} ${duration}`;
+      document.body.appendChild(div);
+      console.log(benchmark.name, duration);
+      // Exclude the first run from the average
+      if (!name.includes("1")) {
+        benchmarkDurationTotals.set(
+          benchmark.name,
+          benchmarkDurationTotals.get(benchmark.name) + duration
+        );
+      }
+      await yieldToBrowser();
+    }
+  };
+
+  for (let i = 1; i <= numRuns; i++) {
+    await runAll(`Run ${i}`);
+  }
+
+  benchmarks.forEach((benchmark) => {
+    const div = document.createElement("div");
+    div.setAttribute("automation", "true");
+    div.innerText = `${benchmark.name} average duration: ${
+      benchmarkDurationTotals.get(benchmark.name) / (numRuns - 1)
+    }`;
+    console.log(
+      `${benchmark.name} average duration: ${
+        benchmarkDurationTotals.get(benchmark.name) / (numRuns - 1)
+      }`
+    );
+    document.body.appendChild(div);
+  });
 }
